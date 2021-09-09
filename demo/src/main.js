b7dfb43c20016dedbd2424244afc958579615895
Tim Fuchs <tim@shakepay.co>
2021-09-09 09:56:08 -0400

    add router

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
-import routes from './routes.js';
+import router from './router.js'
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router: new VueRouter({ routes }),
-  render(h) { return h(App); },
+  router,
+  render(h) { return h(App); }
 }).$mount('#app');
