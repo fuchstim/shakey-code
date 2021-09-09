a823583aec1a112e7c4c7d022d114d0d35c5ddda
Tim Fuchs <tim@shakepay.co>
2021-09-09 12:01:07 -0400

    move

--- src/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const encode = require('./encode');
-const decode = require('./decode');
-
-module.exports = {
-  encode: code => encode(code),
-  decode: buffer => decode(buffer),
-};
